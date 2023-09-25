9e668f92987167d5d6ff892521a553eac14c0a12
monitoria <92344473+gabrielcarvalhopsilva@users.noreply.github.com>
2023-09-25 18:23:26 -0300

    .

--- week6/2.c
+++ week6/2.c
@@ -18,29 +18,58 @@ void printarMatriz(char matriz[LINHAS][COLUNAS]){
     }
 }
 
-void verificarJogada(char matriz[LINHAS][COLUNAS]){
-    int linha, coluna;
-    char simbolo;
-    char m[LINHAS][COLUNAS] = matriz;
+int verificaVencedor(char matriz[LINHAS][COLUNAS], char simbolo) {
+    // Verifica linhas e colunas
+    for(int i = 0; i < LINHAS; i++){
+        if(matriz[i][0] == simbolo && matriz[i][1] == simbolo && matriz[i][2] == simbolo)
+            return 1;
+        if(matriz[0][i] == simbolo && matriz[1][i] == simbolo && matriz[2][i] == simbolo)
+            return 1;
+    }
 
-    scanf("%d %d %c", &linha, &coluna, &simbolo);
+    // Verifica diagonais
+    if(matriz[0][0] == simbolo && matriz[1][1] == simbolo && matriz[2][2] == simbolo)
+        return 1;
+    if(matriz[0][2] == simbolo && matriz[1][1] == simbolo && matriz[2][0] == simbolo)
+        return 1;
 
-
+    return 0;
 }
 
+void verificarJogada(char matriz[LINHAS][COLUNAS], int linha, int coluna, char simbolo){
+    if (matriz[linha][coluna] != '.') {
+        printf("Jogada inválida!\n");
+    } else {
+        matriz[linha][coluna] = simbolo;
 
+        if (verificaVencedor(matriz, simbolo)) {
+            printf("Boa jogada, vai vencer!\n");
+        } else {
+            printf("Continua o jogo.\n");
+        }
+
+        matriz[linha][coluna] = '.'; // Desfaz a jogada para manter a matriz original
+    }
+}
 
 int main(){
-    int jogadas;
     char matriz[LINHAS][COLUNAS];
 
     preencherMatriz(matriz);
     
+    int jogadas;
     scanf("%d", &jogadas);
 
+    for (int i = 0; i < jogadas; i++) {
+        int linha, coluna;
+        char simbolo;
 
+        scanf("%d %d %c", &linha, &coluna, &simbolo);
+        linha--;
+        coluna--;
 
-    
+        verificarJogada(matriz, linha, coluna, simbolo);
+    }
 
     return 0;
 }
