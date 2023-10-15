cbd66911d8249f28d7fe3f438924e30626537abf
Gabriel Carvalho <gabrielcarvalhopsilva@outlook.com>
2023-10-15 07:54:20 -0300

    Update 1.c

--- week5/1.c
+++ week5/1.c
@@ -1,117 +1,104 @@
 #include <stdio.h>
+#include <string.h>
 
-void orderArray(int tamanho, int vetor[]){
-    int temp;
-    for(int i = 0; i < tamanho; i++){
-        for(int j = i+1; j < tamanho; j++){
-            if(vetor[i] > vetor[j]){
-                temp = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = temp;
-            }
+#define TAM 200
+
+char referencial[] =
+    {'0','1','2','3','4','5','6','7','8','9',
+     'A','B','C','D','E','F','G','H','I','J',
+     'K','L','M','N','O','P','Q','R','S','T',
+     'U','V','W','X','Y','Z','.',',','?',' '};
+
+int validaCaracter(char c) {
+    for(int i = 0; i < sizeof(referencial); i++) {
+        if(c == referencial[i]) {
+            return 1;
         }
     }
+    return 0;
 }
 
-void juntarArrays(int v1, int v2, int* vetor1, int* vetor2, int* vetorResultante){
-    int i, j;
-
-    for(i = 0; i < v1; i++){
-        vetorResultante[i] = vetor1[i];
-    }
-    for(j = 0; j < v2; j++){
-        vetorResultante[i+j] = vetor2[j];
-    }
-}
-
-void lerValores(int tamanho, int vetor[]){
-    for(int i = 0; i < tamanho; i++){
-        scanf("%d", &vetor[i]);
-    }
-}
-
-void printarVetor(int tam, int* vetor){
-    for(int i = 0; i < tam; i++){
-        printf("%d ", vetor[i]);
-    }
-}
-
-int verificarQuantidadeRepetidos(int tam, int* vetor){
-    int elementosRepetidos = 0;
-
-    for(int i = 0; i < tam; i++){
-        for(int j = i+1; j < tam; j++){
-            if(vetor[i] == vetor[j]){
-                elementosRepetidos++;
-            }
+void transformaNumeroEmArray(int tamanhoString, int n, int* array){
+    int unidade, dezena, centena, milhar;
+    if(n > 0 && n < 10){
+        unidade = n;
+        for (int i = 0; i < tamanhoString; i++){
+            array[i] = unidade;
         }
-    }
-    return elementosRepetidos * 2;
-}
-
-void verificarElementosRepetidos(int tam, int* vetor, int* vetorRepetidos){
-    int elementosRepetidos = verificarQuantidadeRepetidos(tam,vetor);
-
-    int contadorIndice = 0;
-    int elementoJaExisteNosRepetidos;
-
-    for(int i = 0; i < tam; i++){
-        for(int j = i+1; j < tam; j++){
-            if(vetor[i] == vetor[j]){
-                elementoJaExisteNosRepetidos = 0;
-                for(int k = 0; k < contadorIndice; k++){
-                    if(vetor[i] == vetorRepetidos[k]){
-                        elementoJaExisteNosRepetidos = 1;
-                    }
-                }
-                if(elementoJaExisteNosRepetidos == 0){
-                    vetorRepetidos[contadorIndice]= vetor[i];
-                    contadorIndice++;
-                }
-                
-            }
+    } else if(n >= 10 && n < 100){
+        unidade = n % 10;
+        dezena = n / 10;
+        array[0] = dezena;
+        array[1] = unidade;
+        for (int i = 2; i < tamanhoString; i+=2){
+            array[i] = dezena;
+            array[i+1] = unidade;
+        }
+    } else if(n >= 100 && n < 1000){
+        unidade = n % 10;
+        dezena = (n / 10) % 10;
+        centena = n / 100;
+        array[0] = centena;
+        array[1] = dezena;
+        array[2] = unidade;
+        for (int i = 3; i < tamanhoString; i+=3){
+            array[i] = centena;
+            array[i+1] = dezena;
+            array[i+2] = unidade;
+        }
+    } else if (n >= 1000 && n < 10000){
+        unidade = n % 10;
+        dezena = (n / 10) % 10;
+        centena = (n / 100) % 10;
+        milhar = n / 1000;
+        array[0] = milhar;
+        array[1] = centena;
+        array[2] = dezena;
+        array[3] = unidade;
+        for (int i = 4; i < tamanhoString; i+=4){
+            array[i] = milhar;
+            array[i+1] = centena;
+            array[i+2] = dezena;
+            array[i+3] = unidade;
         }
     }
 }
 
 int main(){
-    int tam1, tam2;
+    int chave;
+    char str[TAM];
 
-    scanf("%d %d", &tam1, &tam2);
-    
-    int conjunto1[tam1];
-    int conjunto2[tam2 ];
-    int tamUniao = tam1 + tam2;
-    int conjuntoUniao[tamUniao];
+    scanf("%d\n", &chave);
+    fgets(str, TAM, stdin);
 
-    lerValores(tam1,conjunto1);
-    lerValores(tam2, conjunto2);
+    char arrayCifra[strlen(str) + 1]; // Adicionando 1 para o caractere nulo no final
 
-    juntarArrays(tam1,tam2,conjunto1,conjunto2,conjuntoUniao);
+    if(chave > 0 && chave < 10000){
+        for(int i = 0; i < strlen(str); i++) {
+            if (!validaCaracter(str[i]) && str[i] != '\n') {
+                printf("Caractere invalido na entrada!\n");
+                return 1;
+            }
 
-    orderArray(tamUniao,conjuntoUniao);
+            int arrayChave[strlen(str)];
+            transformaNumeroEmArray(strlen(str), chave, arrayChave);
 
-    int quantidadeRepetidos = verificarQuantidadeRepetidos(tamUniao, conjuntoUniao);
-    int tamVetorRepetidos = quantidadeRepetidos / 2;
-    int vetorRepetidos[tamVetorRepetidos];
-    int diferencaSimetrica[tamUniao - quantidadeRepetidos];
-    int diferencaSimetricaContador = 0;
-    int elementoRepetido;
-
-    verificarElementosRepetidos(tamUniao, conjuntoUniao, vetorRepetidos);
-
-    for(int i = 0; i < tamUniao; i++){
-        elementoRepetido = 0;
-        for(int j = 0; j < tamVetorRepetidos; j++){
-            if(conjuntoUniao[i] == vetorRepetidos[j]){
-                elementoRepetido = 1;
+            for(int j = 0; j < sizeof(referencial); j++){
+                if(str[i] == referencial[j]){
+                    int indice = (j + arrayChave[i % strlen(str)]) % sizeof(referencial);
+                    arrayCifra[i] = referencial[indice];
+                    break;
+                }
             }
         }
-        if(elementoRepetido == 0){
-            diferencaSimetrica[diferencaSimetricaContador] = conjuntoUniao[i];
-            diferencaSimetricaContador++;
+        arrayCifra[strlen(str)] = '\0'; // Adicionando o caractere nulo no final
+
+        for(int i = 0; i < strlen(arrayCifra)-1; i++){
+            printf("%c", arrayCifra[i]);
         }
+    } else {
+        printf("Chave invalida!");
     }
-    printarVetor(tamUniao - quantidadeRepetidos, diferencaSimetrica);
+
     return 0;
 }
